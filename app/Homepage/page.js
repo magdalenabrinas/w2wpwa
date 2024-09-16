2a45edc91785b469d4fb25cbe75148b4cc1db14b
magdalenabrinas <152439140+magdalenabrinas@users.noreply.github.com>
2024-09-15 22:26:39 -0600

    update the homepage and sw file

--- app/Homepage/page.js
+++ app/Homepage/page.js
@@ -7,7 +7,7 @@ import Link from 'next/link';
 
 const HomePage = () => {
   return (
-    <div className="bg-gradient-to-b from-white/5 to-[#7FC780] min-h-screen flex flex-col items-center justify-center relative">
+    <div className="bg-gradient-to-b from-white/5 to-[#7FC780] min-h-screen flex flex-col items-center justify-between relative p-4">
       {/* Burger Menu */}
       <div className="absolute top-4 left-4">
         <button className="bg-transparent border-none">
@@ -23,60 +23,51 @@ const HomePage = () => {
       </div>
 
       {/* Logo */}
-      <div className="m-4">
-        <img src="/images/logo-small.png" alt="Logo" className="w-[100px] h-[50px]" />
+      <div className="mb-2">
+        <img src="/images/logo-small.png" alt="Logo" className="w-[80px] h-[40px]" />
       </div>
 
       {/* Profile Picture and Username */}
-      <div className="flex items-center">
-        <img src="/images/profile-pic.png" alt="Profile" className="w-12 h-12 rounded-full" />
-        <p className="ml-2 text-lg">Welcome Earth25,</p>
+      <div className="flex items-center mb-2">
+        <img src="/images/profile-pic.png" alt="Profile" className="w-10 h-10 rounded-full" />
+        <p className="ml-2 text-sm">Welcome Earth25,</p>
       </div>
 
       {/* Icons Container */}
-      <div className="my-4">
-        <div className="flex justify-center">
-          <div className="flex">
-            <div className="m-2 text-center">
-              <Link href="/myproject">
-                <a>
-                  <FaBoxArchive className="w-12 h-12 text-black" />
-                  <p className="mt-1 text-sm">My Project</p>
-                </a>
-              </Link>
-            </div>
-            <div className="m-2 text-center">
-              <Link href="/saved">
-                <a>
-                  <CiHeart className="w-12 h-12 text-black" />
-                  <p className="mt-1 text-sm">Saved Project</p>
-                </a>
-              </Link>
-            </div>
-            <div className="m-2 text-center">
-              <Link href="/upcycling">
-                <a>
-                  <BsRecycle className="w-12 h-12 text-black" />
-                  <p className="mt-1 text-sm">Upcycling Guide</p>
-                </a>
-              </Link>
-            </div>
-          </div>
+      <div className="flex flex-wrap justify-center mb-6">
+        <div className="m-2 text-center">
+          <Link href="/myproject">
+            <a>
+              <FaBoxArchive className="w-10 h-10 text-black" />
+              <p className="mt-1 text-xs">My Project</p>
+            </a>
+          </Link>
+        </div>
+        <div className="m-2 text-center">
+          <Link href="/saved">
+            <a>
+              <CiHeart className="w-10 h-10 text-black" />
+              <p className="mt-1 text-xs">Saved Project</p>
+            </a>
+          </Link>
+        </div>
+        <div className="m-2 text-center">
+          <Link href="/upcycling">
+            <a>
+              <BsRecycle className="w-10 h-10 text-black" />
+              <p className="mt-1 text-xs">Upcycling Guide</p>
+            </a>
+          </Link>
         </div>
       </div>
 
       {/* Image Bins Section */}
-      <div className="my-4">
-        <img src="/images/bins.png" alt="Bins" className="w-[100px] h-[100px]" />
-      </div>
-
-      {/* Content */}
-      <div className="flex-1 overflow-auto">
-        {/* Add any other content or components here */}
+      <div className="mb-6">
+        <img src="/images/bins.png" alt="Bins" className="w-[80px] h-[80px]" />
       </div>
 
       {/* Bottom Navigation Bar */}
-      <div className="flex justify-around bg-white p-2 w-full">
+      <div className="flex justify-around bg-white p-2 w-full absolute bottom-0">
         <Link href="/Scanpage">
           <a className="text-center">
             <CameraIcon className="w-6 h-6 text-black" />
